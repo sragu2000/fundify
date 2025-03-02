db411ad70be290ad7ed1f2366868b2ee7801220c
sragu2000 <ragurajsivanantham@gmail.com>
2025-03-02 23:33:56 +0530

    image component updated

--- client/src/components/Image/Image.js
+++ client/src/components/Image/Image.js
@@ -7,19 +7,15 @@ const Image = ({ src, className, id, alt, placeholder, onClick }) => {
 
     return (
         <div className={`relative ${className}`}> {/* Wrapper for styling */}
-            {!isLoaded && !hasError && (
-                <img
-                    src={placeholder || LoadingImage}
-                    alt=""
-                    className={`transition-opacity duration-500 ease-in-out ${className} fallback-image-style`}
-                />
-            )}
+            {/* {!isLoaded && !hasError && (
+                "Loading..."
+            )} */}
             {!hasError ? (
                 <img
                     src={src}
                     id={id}
                     alt={alt || "image"}
-                    loading="lazy"
+                    // loading="lazy"
                     className={`transition-opacity duration-500 ease-in-out ${isLoaded ? "opacity-100" : "opacity-0"} ${className}`}
                     onLoad={() => setIsLoaded(true)}
                     onError={() => setHasError(true)}
@@ -35,3 +31,42 @@ const Image = ({ src, className, id, alt, placeholder, onClick }) => {
 };
 
 export default Image;
+
+
+// import React, { useState } from 'react';
+// import "./image.css"
+// import LoadingImage from "./loading.jpg"
+// const Image = ({ src, className, id, alt, placeholder, onClick }) => {
+//     const [isLoaded, setIsLoaded] = useState(false);
+//     const [hasError, setHasError] = useState(false);
+
+//     return (
+//         <div className={`relative ${className}`}> {/* Wrapper for styling */}
+//             {!isLoaded && !hasError && (
+//                 <img
+//                     src={placeholder || LoadingImage}
+//                     alt=""
+//                     className={`transition-opacity duration-500 ease-in-out ${className} fallback-image-style`}
+//                 />
+//             )}
+//             {!hasError ? (
+//                 <img
+//                     src={src}
+//                     id={id}
+//                     alt={alt || "image"}
+//                     loading="lazy"
+//                     className={`transition-opacity duration-500 ease-in-out ${isLoaded ? "opacity-100" : "opacity-0"} ${className}`}
+//                     onLoad={() => setIsLoaded(true)}
+//                     onError={() => setHasError(true)}
+//                     onClick={onClick}
+//                 />
+//             ) : (
+//                 <div className="flex items-center justify-center w-full h-full bg-gray-200 text-gray-500">
+//                     Image Not Found!
+//                 </div>
+//             )}
+//         </div>
+//     );
+// };
+
+// export default Image;
